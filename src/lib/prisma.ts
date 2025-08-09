4947c4296af797d71cadab18b019503e340446c0
atul23g <atulya.login04@gmail.com>
2025-08-09 17:47:27 +0530

    fix(vercel): run prisma generate on build/postinstall and correct NODE_ENV check

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 declare global{
     // eslint-disable-next-line no-var
@@ -7,4 +7,4 @@ declare global{
 
 export const client = globalThis.prisma || new PrismaClient()
 
-if (process.env.Node_ENV !== 'production') globalThis.prisma = client
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = client
